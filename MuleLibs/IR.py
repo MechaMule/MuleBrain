1bed4b8c65f45a419c34edd5b4d04465c387f9a5
Johnson Le <jonle@ucsc.edu>
2019-01-31 14:46:35 -0800

    added comments and some cleanup code

--- MuleLibs/IR.py
+++ MuleLibs/IR.py
@@ -8,17 +8,25 @@
 # IMPORTS
 #===============================================================================
 import RPi.GPIO as IO
-from enum import Enum
 
+#===============================================================================
+# Class:    IR
+# Desc:     Class for IR. Should only have one instance.
+# Param:    none atm. Might add pin param init.
+#===============================================================================
 class IR:
-    _instances=[]
     def __init__(self):
-        if(len(self._instances) >= 1):
-            print("ERROR: Only one instance of IR should be made.")
-            IO.cleanup()
-            raise SystemExit
-        self._instances.append(self)
-    
+        if getattr(self.__class__, '_has_instance', False):
+            raise RuntimeError('Cannot create another instance')
+        self.__class__._has_instance = True
+        
+    #===============================================================================
+    # Function: Init(self)
+    # Desc:     Initialize IR. Sets pin active input with software pull down
+    #           resistor.
+    # Params:   none
+    # Returns:  none, will stop program if bad.
+    #===============================================================================
     def Init(self):
         mode = IO.getmode()
         if mode == IO.BOARD:
@@ -40,22 +48,37 @@ class IR:
             print("IR ERROR: Could not initialize. Is GPIO mode set?")
             IO.cleanup()
             raise SystemExit
-        
+
+    #===============================================================================
+    # Function: Status(self)
+    # Desc:     Reads the status of all the pins
+    # Params:   none
+    # Returns:  4-bit number. 0bNWES
+    #===============================================================================
     def Status(self):
-        self.state_N = IO.input(self.pin_N)
-        self.state_S = IO.input(self.pin_S)
-        self.state_E = IO.input(self.pin_E)
-        self.state_W = IO.input(self.pin_W)
-        return (self.state_N<<3 | self.state_W<<2 | self.state_E<<1 | self.state_S)
+        try:
+            self.state_N = IO.input(self.pin_N)
+            self.state_S = IO.input(self.pin_S)
+            self.state_E = IO.input(self.pin_E)
+            self.state_W = IO.input(self.pin_W)
+        except:
+            print("ERROR: Could not read pin states. Has pin or GPIO mode been set?")
+            IO.cleanup()
+            raise SystemExit
+        
+        return ((self.state_N<<3 | self.state_W<<2 | self.state_E<<1 | self.state_S) & 0b1111)
 
     def Clean(self):
         IO.cleanup()
-        
+
+
+#===============================================================================
+# Testing code
+#===============================================================================
 if __name__ == "__main__":
     IO.setwarnings(False)
     IO.setmode(IO.BCM)
 
-    
     IR = IR()
     IR.Init()
     print(IR.Status())
