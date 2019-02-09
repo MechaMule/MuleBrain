f4cf17576d05f4e0acd842251109e260d686dbdc
Johnson <johnnjacob14@yahoo.com>
2019-02-09 14:27:29 -0800

    modified pwm to take in freq, duty, and a pin when creating the object

--- MuleLibs/pwm.py
+++ MuleLibs/pwm.py
@@ -12,54 +12,31 @@ import RPi.GPIO as IO
 #===============================================================================
 # Class:    PWM
 # Desc:     Class for PWM
-# Param:    ch = channel. There are 3 pwm channels.
+# Param:    pin = pin number. There are 3 main pwm channels.
 #           for board: ch1 = pin12, ch2 = pin32, ch3 = pin33
 #           for bcm: bcm18,12,13
 #===============================================================================
-class PWM:
-    def __init__(self, ch):
-        #mapping channels to gpio pins
-        if ch > 3:
-            print("ERROR: Only 3 PWM channels.")
-            raise SystemExit
-        self.ch = ch
-
+class PWM(object):
     #===============================================================================
-    # Function: Init(self, f)
-    # Desc:     Initialize PWM. Sets pin active. Starts at 0% duty cycle.
-    # Params:   f = frequency that pwm will be set too.
-    # Returns:  TRUE means success. FALSE = failed.
+    # Function: PWM constructor
+    # Desc:     create the pwm object with some parameters
+    # Params:   f = frequency for pwm
+    #           pin = what pin
+    #           duty = duty cycle
+    # Returns:  Nothing. Might add TRUE means success. False = failed.
     #===============================================================================
-    def Init(self, f):
-        self.freq = f
-        mode = IO.getmode()
-        if mode == IO.BOARD:  
-            if self.ch == 1:
-                self.pin = 12
-            elif self.ch == 2:
-                self.pin = 32
-            elif self.ch == 3:
-                self.pin = 33
-        elif mode == IO.BCM:
-            if self.ch == 1:
-                self.pin = 18
-            elif self.ch == 2:
-                self.pin = 12
-            elif self.ch == 3:
-                self.pin = 13
-        
+    def __init__(self, pin, freq = 1000, duty = 0):
+        self.pin = pin
+        self.freq = freq
+        self.duty = duty
         try:
             IO.setup(self.pin, IO.OUT)
         except:
-            print("ERROR: Could not initialize PWM. Is GPIO mode set?")
+            print("ERROR: Could not initialize this PWM. IS GPIO mode set?")
             IO.cleanup()
             raise SystemExit
-            return False    #doesn't really get here but ye(:
-        
         self.pwm = IO.PWM(self.pin, self.freq)
-        self.pwm.start(0)
-        self.duty = 0
-        return True
+        self.pwm.start(self.duty)
 
     #===============================================================================
     # Function: PWM_SetFrequency(self, f)
@@ -118,17 +95,6 @@ if __name__ == "__main__":
     print("Running PWM test code")
     IO.setwarnings(False)
     IO.setmode(IO.BCM)
-
-    pwm1 = PWM(1)
-    pwm2 = PWM(3)
-    pwm1.Init(100)
-    pwm2.Init(1000)
-    pwm1.SetDutyCycle(50)
-    pwm2.SetDutyCycle(50)
-
+    pwm = PWM(18)
     input("press return to stop.")
-
-    pwm1.Stop()
-    pwm2.Stop()
-
-    IO.cleanup()
+    pwm.Clean()
