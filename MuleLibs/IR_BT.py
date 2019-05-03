91c7b5aa3e6d75c372f1151cf964296888fbccdd
Daniel <daaraujo@ucsc.edu>
2019-05-02 20:53:32 -0700

    Made a couple of changes to the files format, getting it ready for actual test cases. Also I'm trying to make its output easier to read.

--- MuleLibs/IR_BT.py
+++ MuleLibs/IR_BT.py
@@ -3,11 +3,8 @@ import time
 import RPi.GPIO as GPIO
 
 #===================== Predefined messages =======================
-Left_Detect = "SIGNAL_FOUND_L"
-Left_Lost   = "SIGNAL_LOST_L"
-
-Right_Detect = "SIGNAL_FOUND_R"
-Right_Lost   = "SIGNAL_LOST_R"
+Detect = "FOUND"
+Lost   = "LOST"
 
 Both_Detect  = "BOTH_FOUND"
 Both_Lost    = "BOTH_LOST"
@@ -22,45 +19,34 @@ GPIO.setup(IR_R, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 received = False
 
-def data_received(data):
-    print("received: ",data)
-    c.send(data)
-    global received
-    received = True
-
 # For now the callbacks have print statements to check to see if they
 #are being accessed
 def IRL_Callback(channel):
     if (GPIO.input(IR_L) == GPIO.LOW) and (GPIO.input(IR_R) == GPIO.LOW):
-        #PiZero_Client.send(Both_Detect)
-        print('Found_BOTH_L_Callback')
+        print("{}_L_Callback".format(Both_Detect))
         
     elif (GPIO.input(IR_L) == GPIO.HIGH) and (GPIO.input(IR_R) == GPIO.HIGH):
-        print('Lost BOTH_L_Callback')
+        print("{}_L_Callback".format(Both_Lost))
         
-    elif (GPIO.input(IR_L) == GPIO.HIGH) and (GPIO.input(IR_R) == GPIO.LOW):       #Need to do a check for both HIGH and then move this statement down the
-        #PiZero_Client.send(Left_Detect)        #of if statements
-        print('Lost LEFT_L_Callback')
-    else:
-        #PiZero_Client.send(Left_Lost)
-        print('Lost_RIGHT_L_Callback')
+    elif (GPIO.input(IR_L) == GPIO.HIGH) and (GPIO.input(IR_R) == GPIO.LOW): 
+        print("{}_LEFT_L_Callback".format(Lost))
+    else:                               # Might not need this statument
+        #PiZero_Client.send(Left_Lost)  # Because it is redudent with statement
+        print("{}_RIGHT_L_Callback".format(Lost))  # in IRR_Callback
 
         
 def IRR_Callback(channel):
     if (GPIO.input(IR_R) == GPIO.LOW) and (GPIO.input(IR_L) == GPIO.LOW):
-        #PiZero_Client.send(Both_Detect)
-        print('Found BOTH_R_Callback')
+        print("{}_R_Callback".format(Both_Detect))
 
     elif (GPIO.input(IR_R) == GPIO.HIGH) and (GPIO.input(IR_L) == GPIO.HIGH):
-        print('Lost BOTH_R_Callback')
+        print("{}_R_Callback".format(Both_Lost))
     
     elif (GPIO.input(IR_R) == GPIO.HIGH) and (GPIO.input(IR_L) == GPIO.LOW):
-        #PiZero_Client.send(Right_Detect)
-        print('Lost RIGHT_R_callback')
+        print("{}_RIGHT_R_callback".format(Lost))
         
     else:
-        #PiZero_Client.send(Right_Lost)
-        print('Lost LEFT_R_Callback')
+        print("{}_LEFT_R_Callback".format(Lost))
 
 
 GPIO.add_event_detect(IR_L, GPIO.BOTH, callback=IRL_Callback, bouncetime=300)
@@ -68,26 +54,14 @@ GPIO.add_event_detect(IR_R, GPIO.BOTH, callback=IRR_Callback, bouncetime=300)
 
 if __name__ == "__main__":
     print("==================== Starting IR Message Test =====================")
-#    PiZero_Client = BluetoothClient("johnpi", data_received)
 
     try:
         
         while True:
-#            #Signals are inverted 
-#            if (GPIO.input(IR_L) == GPIO.LOW) and (GPIO.input(IR_R) == GPIO.LOW):
-#                print('Both sensors found signal')
-#            
-#            elif GPIO.input(IR_L) == GPIO.LOW:            
-#                print('Only left found signal(HIGH), should send message') #these are
-#                
-#            elif GPIO.input(IR_R) == GPIO.LOW:
-#                print('Only right found signal(HIGH), message sent')
-#                
-#            else:
-#                print('Both lost signal')
-            #This displays the status of booth sensors 
-            print("L_Status: {} | R_Status: {}".format( GPIO.input(IR_L),  GPIO.input(IR_R)))
 
+            # Shows us the status of the IR sensors in real semi-real time.
+            print("L_Status: {} | R_Status: {}".format( GPIO.input(IR_L),  GPIO.input(IR_R) ))
+            # Slight delay to releave the processor of any stress. 
             time.sleep(1/2)
 
 
