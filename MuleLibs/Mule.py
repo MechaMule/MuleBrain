a25dea6ab22ed97a937fb6594da06d13d2be7ce5
Johnson Le <jonle@ucsc.edu>
2019-05-04 21:06:32 -0700

    motor feedback loop implemented for testing

--- MuleLibs/Mule.py
+++ MuleLibs/Mule.py
@@ -21,20 +21,21 @@ class MULE(object):
     (3) pin_trig   : Int. Trigger pin
     (4) pin_ECHOS  : Array. Holds all echo pins. Ideally from left to right
     """
-    def __init__(self, killswitch, pin_MTRS, pin_ECHOS):
+    def __init__(self, killswitch, pin_MTRS, pin_ECHOS, eargap=13/12):
         #store pins. might make them enum arrays.
         self.killswitch = killswitch
         self.pin_MTRS = pin_MTRS
         self.pin_ECHOS = pin_ECHOS
         self.echo = []
+        self.eargap = 13
 
         #motor
         self.MTR = MOTOR(self.pin_MTRS[0], self.pin_MTRS[1], self.pin_MTRS[2], self.pin_MTRS[3])
-        
+
         #USR
         for i in range(0, len(self.pin_ECHOS)):
             self.echo.append(Echo.ECHO(self.pin_ECHOS[i]))
-        
+
     def clean(self):
         """Cleans up the echo and motor pins"""
         print("Initiating mule cleansing.")
@@ -46,35 +47,34 @@ class MULE(object):
 
 if __name__ == '__main__':
     print("Mule says hi")
-    def MapRange(x, in_min, in_max, out_min, out_max):
-        return (out_min + ( ((out_max-out_min)*(x-in_min))/(in_max-in_min) ))
+    def MapRange(x, old, new):
+        if x<=old[0]:
+            return new[0]
+        elif x>=old[1]:
+            return new[1]
+        return(new[0] + ( ((new[1]-new[0])*(x-old[0]))/(old[1]-old[0]) ))
+
     try:
         killswitch = threading.Event()
         mule = MULE(killswitch, [13,6,26,19], [20,21])
         mule.MTR.Halt()
-        dl = 0
-        dr = 0
-        minD = 2
-        maxD = 3
-        minspd = 20
-        maxspd = 98
+
+        Mmax = 100
+        Mmin = 0
+        dGoal = 5
+        Kp = 1.5
+        Bmax = Mmax - (Kp*mule.eargap)
+
         while True:
-            time.sleep(0.1)
-            dl = mule.echo[0].GetFeet()
-            dr = mule.echo[1].GetFeet()
-            print("LEFT: ",dl,"   ||   RIGHT: ",dr)
-            if dl<minD:
-               mule.MTR.Motor_L(0)
-            elif dl<maxD:     
-                mule.MTR.Motor_L(MapRange(dl, minD, maxD, minspd, maxspd))
-                
-            if dr<minD:
-               mule.MTR.Motor_R(0)
-            elif dr<maxD:    
-                mule.MTR.Motor_R(MapRange(dr, minD, maxD, minspd, maxspd))
-            
-##            print(mule.echo[0].GetInch())
-            
+            time.sleep(0.2)
+            dL = mule.echo[0].GetFeet()
+            dL = mule.echo[1].GetFeet()
+            dc = (dL+dR)/2
+            mb = MapRage(dc, [dGoal, dGoal+Kp], [Mmin,Mmax])
+
+            mL = mb + Kp*(0-(dL-dR))
+            mR = mb = Kp*(0-(dR-dL))
+
 
     except KeyboardInterrupt:
         print("pressed ctrl+c")
