83c52a7ab7b93625db17f2a010dfcda472c63a5a
John <jonle@ucsc.edu>
2019-05-29 23:39:03 -0700

    uh

--- MuleLibs/Mule.py
+++ MuleLibs/Mule.py
@@ -11,80 +11,167 @@ import Echo
 import Signal
 import threading
 import time
+import matplotlib.pyplot as plt
+from simple_pid import PID
 from MuleMotor_Functions import MOTOR
 
 class MULE(object):
-    def __init__(self, pin_dirL, pin_dirR, pin_mtrL, pin_mtrR, \
-                 pin_trig, pin_echoL1, pin_echoL2, pin_echoR1, pin_echoR2, \
-                 killswitch, speed_default=20, speed_max=100, \
-                 speed_max_offset=30, dist_bot=5, \
-                 trig_hi=10E-6, trig_lo=60E-3):
-
+    """Holds pins and setings for Mule.
+    Parameters:
+    (1) killswitch : Thread event to close all threads
+    (2) pin_MTRS   : Array holding motor pins. enL, enR, dirL, dirR
+    (3) pin_trig   : Int. Trigger pin
+    (4) pin_ECHOS  : Array. Holds all echo pins. Ideally from left to right
+    """
+    def __init__(self, killswitch, pin_MTRS, pin_ECHOS, eargap=13/12):
         #store pins. might make them enum arrays.
-        self.pin_dirL = pin_dirL
-        self.pin_dirR = pin_dirR
-        self.pin_mtrL = pin_mtrL
-        self.pin_mtrR = pin_mtrR
-        self.pin_trig = pin_trig
-        self.pin_echoL1 = pin_echoL1
-        self.pin_echoL2 = pin_echoL2
-        self.pin_echoR1 = pin_echoR1
-        self.pin_echoR2 = pin_echoR2
         self.killswitch = killswitch
-
-        #main settings for 
-        self.speed_default = speed_default
-        self.speed_max = speed_max
-        self.speed_max_offset = speed_max_offset
-        self.dist_bot = dist_bot
-        self.trig_hi = trig_hi
-        self.trig_lo = trig_lo
-
-        #ping stuff. need to start trigger. echo works right away.
-        self.trigger = Signal.Generator(self.killswitch, self.pin_trig, self.trig_hi, self.trig_lo)
-##        self.pulser = Signal.Pulser(self.killswitch, self.pin_trig, self.trig_hi, self.trig_lo)
-##        self.echoL1 = Echo.ECHO(self.pin_echoL1)
-##        self.echoR1 = Echo.ECHO(self.pin_echoR1)
-##        self.echoL2 = Echo.ECHO(self.pin_echoL2)
-##        self.echoR2 = Echo.ECHO(self.pin_echoR2)
+        self.pin_MTRS = pin_MTRS
+        self.pin_ECHOS = pin_ECHOS
+        self.echo = []
+        self.eargap = 13
 
         #motor
-##        self.MTR = MOTOR(self.pin_mtrL, self.pin_mtrR, self.pin_dirL, self.pin_dirR)
+        self.MTR = MOTOR(self.pin_MTRS[0], self.pin_MTRS[1], self.pin_MTRS[2], self.pin_MTRS[3])
+
+        #motor controllers
+##        self.pidL = PID(0, 0, 0, setpoint=0)
+##        self.pidR = PID(0, 0, 0, setpoint=0)
         
+        #USR
+        for i in range(0, len(self.pin_ECHOS)):
+            self.echo.append(Echo.ECHO(self.pin_ECHOS[i]))
+
+
     def clean(self):
+        """Cleans up the echo and motor pins"""
         print("Initiating mule cleansing.")
-##        self.echoL1.clean()
-##        self.echoL2.clean()
-##        self.echoR1.clean()
-##        self.echoR2.clean()
-##        self.MTR.clean()
-            
-        
-        
+        for i in range(0, len(self.pin_ECHOS)):
+            self.echo[i].clean()
+        self.MTR.clean()
+
+
 
 if __name__ == '__main__':
     print("Mule says hi")
-    killswitch = threading.Event()
-    mule = MULE(26, 19, 13, 6, \
-                17, 27, 22, 23, 24, \
-                killswitch)
-##    mule.trigger.start()
-    mule.MTR.Halt()
-    mule.MTR.Motor_L(-30)
-    mule.MTR.Motor_R(-80)
-
+    import math
+    f = open("../Log/log.txt", "w+")
+    def MapRange(x, old, new):
+        out = (new[0] + ( ((new[1]-new[0])*(x-old[0]))/(old[1]-old[0]) ))
+        if out <= new[0]:
+            return new[0]
+        elif out >= new[1]:
+            return new[1]
+        return out
     try:
+        killswitch = threading.Event()
+        mule = MULE(killswitch, [6,13,19,26], [20,21])
+        mule.MTR.Halt()
+
+        Kp, Ki, Kd = 10, 0, 0
+        
+        Mmax = 69
+        Mmin = 50
+        dGoal = 5
+        idle = True
+        mb = mL = mR = 0
+        dL = dR = 0
+        adjL = adjR = 0
+        motor_offset = 0
+
+        pidL = PID(Kp, Ki, Kd, setpoint=0)
+        pidR = PID(Kp, Ki, Kd, setpoint=0)
+
+        pidL.tunings = (60, 0.5, 5)
+        pidR.tunings = (60, 0.5, 5)
+        pidL.output_limits = (-66, 66)
+        pidR.output_limits = (-66, 66)
+        
+        pidL.auto_mode = True
+        pidR.auto_mode = True
+
+        x_axis, y_mL, y_mR, y_dL, y_dR = [], [], [], [], []
+        
+        time.sleep(0.5)
+        iteration = 0
+        sample_t = 1E-1
         while True:
-            input("enter to pulse")
-            mule.pulser.Pulse(3)
+            time.sleep(sample_t)
+            dL = mule.echo[0].GetFeet()
+            dR = mule.echo[1].GetFeet()
+            dc = (dL+dR)/2
+            if idle==True:
+                if dc >= dGoal + 2:
+                    idle = False
+                    pidL.tunings = (8, 0, 1.5)
+                    pidR.tunings = (8, 0, 1.5)
+                    pidL.output_limits = (-8, 8)
+                    pidR.output_limits = (-8, 8)
+                    mb = 60
+                else:
+                    mb = 0
+                    adjL = pidL(dR-dL)
+                    adjR = pidR(dL-dR)
+            else:
+                if dc >= dGoal:
+##                    mb = MapRange(dc, [dGoal, dGoal+10], [Mmin,Mmax])
+                    mb = 40
+                    adjL = pidL(dR-dL)
+                    adjR = pidR(dL-dR)-3
+                    
+##                    mule.MTR.Motor_L((mb + Kp*(0-(dR-dL))))
+##                    mule.MTR.Motor_R((mb + Kp*(0-(dL-dR))) - motor_offset)
+                else:
+                    mL = 0
+                    mR = 0
+                    mb = 0
+                    mule.MTR.Halt()
+                    idle = True
+                    pidL.tunings = (60, 0.5, 5)
+                    pidR.tunings = (60, 0.5, 5)
+                    pidL.output_limits = (-66, 66)
+                    pidR.output_limits = (-66, 66)
+                                
+            mL = mb + adjL
+            mR = mb + adjR
+            mule.MTR.Motor_L(mL)
+            mule.MTR.Motor_R(mR)
+            
+            x_axis += [round(iteration * sample_t,3)]
+            y_dL.append(round(dL,3))
+            y_dR.append(round(dR,3))
+            y_mL.append(round(mL,3))
+            y_mR.append(round(mR,3))
+            iteration += 1 
+            
+            print("Distance: ", round(dL,3), "   ||   ", round(dR,3))
+            
+##            print("d: [",dL,",",dR,"]   ||   m: [",round(mule.MTR.GetLSpeed(),3),",",round(mule.MTR.GetRSpeed(),3),"]")
+##            # f.write(str(dL)+","+str(dR)+","+str(round(mule.MTR.GetLSpeed(),3))+","+str(round(mule.MTR.GetRSpeed(),3))+"\n")
+
+
+
     except KeyboardInterrupt:
-        pass
-    finally:
+        print("pressed ctrl+c")
+        f.close()
         mule.MTR.Halt()
         killswitch.set()
         mule.clean()
-        time.sleep(0.5)
-            
-    
-
         
+        plt.subplot(2,1,1)
+        plt.plot(x_axis, y_dL, label='dL')
+        plt.plot(x_axis, y_dR, label='dR')
+        plt.xlabel('time')
+        plt.ylabel('distance')
+        plt.legend()
+
+        plt.subplot(2,1,2)
+        plt.plot(x_axis, y_mL, label='mL')
+        plt.plot(x_axis, y_mR, label='mR')
+        plt.xlabel('time')
+        plt.ylabel('Motor Speed')
+        plt.legend()
+        plt.show()
+    finally:
+        print("goodbye")
+        time.sleep(0.5)
